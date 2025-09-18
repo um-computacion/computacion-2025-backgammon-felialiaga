f4ff974408d871812b7e36e433499556b6bf70eb
Felipe Aliaga <felialiagaa@gmail.com>
2025-09-18 11:47:27 -0300

    Agregue la clase Checker con sus metodos

---
+++ core/Checker.py
@@ -0,0 +1,36 @@
+from Player import Player
+
+class Checker:
+    def __init__(self, color, player: Player, position):
+        self.__color = color
+        self.__player = player
+        self.__position = position
+        self.__middle = False
+        self.__out = False
+
+    def get_color(self):
+        return self.__color
+    
+    def get_position(self):
+        return self.__position
+
+    def set_position(self, position):
+        self.__position = position
+
+    def validate_position(self):
+        pass
+
+    def move(self, position):
+        self.set_position(position)
+
+    def move_to_middle(self):
+        self.__position = None
+        self.__middle = True
+
+    def move_out(self):
+        self.__position = None
+        self.__out = True
+
+
+    def reset(self):
+        pass
