bfeb9b48e9f4b7b9df772f3cb98ff50c22507985
Felipe Aliaga <felialiagaa@gmail.com>
2025-10-29 14:08:18 -0300

    feat: se implemento el uso de la direccion del jugador

--- core/Player.py
+++ core/Player.py
@@ -1,14 +1,17 @@
 
 class Player:
-    def __init__(self, name, number):
+    def __init__(self, name, number, direction):
         self.__name = name
         self.__number = number
         self.__token = ""
+        self.__direction = direction
 
         if number == 1:
             self.__token = "X"
+            self.__direction = 1
         elif number == 2:
             self.__token = "O"
+            self.__direction = -1
         else:
             print('Debe ingresar o un 1 o un 2')#Lanzar excepcion IncorrectNumberPlayer
 
@@ -40,3 +43,7 @@ class Player:
     @property
     def token(self):
         return self.__token
+    
+    @property
+    def direction(self):
+        return self.__direction
