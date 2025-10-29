fd14d4751d12367a0fd4e8179f519ad988751e55
Felipe Aliaga <felialiagaa@gmail.com>
2025-10-29 13:59:19 -0300

    feat: cambios en la estructura de la clase e integracion de funcion duplicate

--- core/Dice.py
+++ core/Dice.py
@@ -5,23 +5,14 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 class Dice:
     def __init__(self):
-        self.__dice1 = 0
-        self.__dice2 = 0
-        self.__total = 0
+        self.__values = [0,0]
 
     def throw_dice(self):
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
         
-        if(dice1 == dice2):
-            total = dice1 * 4
-        
-        total = dice1 + dice2
-
-        self.__dice1 = dice1
-        self.__dice2 = dice2
-        self.__total = total
-
+        self.__values = [dice1, dice2]
+        return self.__values
 
     def get_dice1(self):
        
@@ -31,9 +22,9 @@ class Dice:
         
         return self.__dice2
     
-    def get_total(self):
-
-        return self.__total
+    def doubles(self):
+        #Esta funcion devolvera True si los 2 dados son iguales
+        return self.__values[0] == self.__values[1]
 
     def set_dice1(self, number):
         self.__dice1 = int(number)
@@ -41,15 +32,12 @@ class Dice:
     def set_dice2(self, number):
         self.__dice2 = int(number)
 
-    def set_total(self, dice1, dice2):
-
-        if(dice1 == dice2):
-            total = dice1 * 4
+    def duplicate(self):
+        #Este metodo se usara para saber los movimientos que tenemos, si son iguales los duplica, sino devuelve los valores de los 2 dados
+        if self.doubles():
+            return[self.__values[0] * 4]
         else:
-            total = dice1 + dice2
-
-        self.__total = total
-
+            return self.__values
 
     def __str__(self):
         return f"Valor dado 1: {self.__dice1}, Valor dado 2: {self.__dice2}, Valor Total: {self.__total}"
